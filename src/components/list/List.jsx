08d736cacdf01f99bf5160eba5e8f2a078581cea
Ionela Pascaru <ipascaru@c.fitbit.com>
2023-04-11 18:58:46 +0300

    carousel wip

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,20 +1,46 @@
 import "./list.scss";
+import { useRef, useState } from "react";
 import ArrowBackIosNewOutlinedIcon from '@mui/icons-material/ArrowBackIosNewOutlined';
 import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined';
 import ListItem from '../listItem/ListItem';
 
 const List = () => {
+    const [isMoved, setIsMoved] = useState(false);
+    const [sliderNumber, setSliderNumber] = useState(0);
+    const listRef = useRef();
+
+    const handleClick = (direction) => {
+        setIsMoved(true);
+        let distance = listRef.current.getBoundingClientRect().x - 50;
+        if (direction === "left" && sliderNumber > 0) {
+            setSliderNumber(sliderNumber - 1)
+            listRef.current.style.transform = `translateX(${distance + 230}px)`;
+        }
+        if (direction === "right" && sliderNumber < 2) {
+            setSliderNumber(sliderNumber + 1)
+            listRef.current.style.transform = `translateX(${distance - 230}px)`;
+        }
+        console.log(distance);
+    }
+
     return (
         <div className="list">
             <span className="listTitle">Title</span>
             <div className="wrapper">
-                <ArrowBackIosNewOutlinedIcon className="sliderArrow left"/>
-                <div className="container">
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
+                <ArrowBackIosNewOutlinedIcon className="sliderArrow left" onClick={() => handleClick("left")} 
+                    style={{display: !isMoved && "none"}}
+                />
+                <div className="container" ref={listRef}>
+                    <ListItem index={0} />
+                    <ListItem index={1} />
+                    <ListItem index={2} />
+                    <ListItem index={3} />
+                    <ListItem index={4} />
+                    <ListItem index={5} />
+                    <ListItem index={6} />
+                    <ListItem index={7} />
                 </div>
-                <ArrowForwardIosOutlinedIcon className="sliderArrow right"/>
+                <ArrowForwardIosOutlinedIcon className="sliderArrow right" onClick={() => handleClick("right")} />
             </div>
         </div>
     )
--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,9 +1,37 @@
 import "./listItem.scss";
+import { useState } from "react";
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import AddIcon from '@mui/icons-material/Add';
+import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
+import ThumbDownOutlinedIcon from '@mui/icons-material/ThumbDownOutlined';
 
-const ListItem = () => {
+const ListItem = ({ index }) => {
+  const [isHovered, setIsHoverd] = useState(false);
+  const trailer = "";
   return (
-    <div className="listItem">
-      item
+    <div className="listItem" style={{ left: isHovered && index * 225 - 50 + index * 2.5 }} onMouseEnter={() => setIsHoverd(true)} onMouseLeave={() => setIsHoverd(false)}>
+      <img src="https://fastly.picsum.photos/id/866/536/354.jpg?hmac=tGofDTV7tl2rprappPzKFiZ9vDh5MKj39oa2D--gqhA" alt="" />
+      {isHovered && (<>
+        <video src={trailer} autoPlay={true} loop />
+        <div className="itemInfo">
+          <div className="icons">
+            <PlayArrowIcon className="icon" />
+            <AddIcon className="icon" />
+            <ThumbUpOutlinedIcon className="icon" />
+            <ThumbDownOutlinedIcon className="icon" />
+          </div>
+          <div className="itemInfoTop">
+            <span>1h 14min</span>
+            <span className="limit">+16</span>
+            <span>1999</span>
+          </div>
+          <div className="desc">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum ducimus ex temporibus.
+        </div>
+          <div className="genre"></div>
+        </div>
+      </>)
+      }
     </div>
   )
 }
